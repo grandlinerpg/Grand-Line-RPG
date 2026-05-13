12559f85a25493b171a97cf370429d11d4c87d30
grandlinerpg <drivedesuna@gmail.com>
2026-05-13 14:16:24 -0300

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -123,9 +123,15 @@ async function startAuth() {
       const expMax = 1000;
 
       document.getElementById("level").innerText = level;
-      document.getElementById("exp").innerText = exp;
-      document.getElementById("exp-max").innerText = expMax;
-      document.getElementById("saldo").innerText = saldo;
+      
+      document.getElementById("exp").innerText =
+        exp.toLocaleString("pt-BR");
+      
+      document.getElementById("exp-max").innerText =
+        expMax.toLocaleString("pt-BR");
+      
+      document.getElementById("saldo").innerText =
+        saldo.toLocaleString("pt-BR");
 
       // ======================
       // ATRIBUTOS

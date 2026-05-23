841d845d2f96add39a89ddeef486fcf4e3bb8ca8
grandlinerpg <drivedesuna@gmail.com>
2026-05-23 19:45:26 -0300

    Update inventario.js

--- perfil/inventario.js
+++ perfil/inventario.js
@@ -3,6 +3,8 @@ import {
   get
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+import { usarItem } from "./item.js"; // 🔥 INTEGRAÇÃO
+
 const modalContainer = document.getElementById("modal-container");
 
 let currentItem = null;
@@ -161,13 +163,13 @@ function initInventory(){
   });
 
   // =========================
-  // USAR ITEM (SEM item.js)
+  // USAR ITEM (AGORA COM item.js)
   // =========================
 
   useBtn.addEventListener("click", async () => {
     if (!currentItem) return;
 
-    console.log("USAR ITEM:", currentItem);
+    await usarItem(currentItem);
 
     itemModal.style.display = "none";
   });

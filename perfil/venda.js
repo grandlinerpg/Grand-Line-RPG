ead01f55a6440cfd05f51de891abbbe471246468
grandlinerpg <drivedesuna@gmail.com>
2026-07-17 18:50:27 -0300

    Update venda.js

--- perfil/venda.js
+++ perfil/venda.js
@@ -147,7 +147,7 @@ qtyInput.value = 1;
   imgBox.innerHTML = item.img
   ? `
     <img
-      class="inventory-open-img"
+      class="inventory-item-img"
       src="https://res.cloudinary.com/djh45admn/image/upload/v1778432202/${item.img}.png"
     >
   `

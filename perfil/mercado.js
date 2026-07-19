9471f007425507198d57e4556b7c753856b179cd
grandlinerpg <drivedesuna@gmail.com>
2026-07-18 23:28:56 -0300

    Update mercado.js

--- perfil/mercado.js
+++ perfil/mercado.js
@@ -373,7 +373,7 @@ function initMarket() {
       const nomeJogador = await getNomeJogador(marketItem.jogador);
 
       sellerText.innerText =
-        `Anunciado por ${marketItem.nomeJogador || "Jogador"}`;
+        `Anunciado por ${nomeJogador || "Jogador"}`;
 
     }
 

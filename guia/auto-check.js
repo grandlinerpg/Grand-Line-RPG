3ff35faf0f337b015752ef22562e23e4005333bd
grandlinerpg <drivedesuna@gmail.com>
2026-07-22 09:11:33 -0300

    Create auto-check.js

---
+++ guia/auto-check.js
@@ -0,0 +1,66 @@
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+import {
+  getAuth,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyC4kgy_L79WYFQR9XZhoDuZBfqG4AGTVUQ",
+
+  authDomain: "grand-line-rpg-dcda9.firebaseapp.com",
+
+  projectId: "grand-line-rpg-dcda9",
+
+  storageBucket: "grand-line-rpg-dcda9.appspot.com",
+
+  messagingSenderId: "172042779786",
+
+  appId: "1:172042779786:web:ecdff9eaf4fee36eca8173",
+
+  databaseURL: "https://grand-line-rpg-dcda9-default-rtdb.firebaseio.com"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+
+const homeLink =
+document.getElementById("home-link");
+
+
+
+onAuthStateChanged(auth,(user)=>{
+
+
+  console.log("AUTH CHECK:", user);
+
+
+  if(!homeLink) return;
+
+
+  if(user){
+
+    homeLink.textContent = "PERFIL";
+    homeLink.href = "perfil.html";
+
+  }else{
+
+    homeLink.textContent = "INÍCIO";
+    homeLink.href = "index.html";
+
+  }
+
+
+});

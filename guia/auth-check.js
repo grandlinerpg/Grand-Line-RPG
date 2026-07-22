1e51e4f2e49d44cadf1a85bc22d24b45c4298ad1
grandlinerpg <drivedesuna@gmail.com>
2026-07-22 09:38:47 -0300

    Update auth-check.js

--- guia/auth-check.js
+++ guia/auth-check.js
@@ -5,91 +5,75 @@ import {
 
 import {
   getAuth,
-  onAuthStateChanged
+  onAuthStateChanged,
+  setPersistence,
+  browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-
-
-// ======================
-// FIREBASE CONFIG
-// ======================
 
 const firebaseConfig = {
 
   apiKey: "AIzaSyC4kgy_L79WYFQR9XZhoDuZBfqG4AGTVUQ",
 
-  authDomain: "grand-line-rpg-dcda9.firebaseapp.com",
+  authDomain:"grand-line-rpg-dcda9.firebaseapp.com",
 
-  projectId: "grand-line-rpg-dcda9",
+  projectId:"grand-line-rpg-dcda9",
 
-  storageBucket: "grand-line-rpg-dcda9.appspot.com",
+  storageBucket:"grand-line-rpg-dcda9.appspot.com",
 
-  messagingSenderId: "172042779786",
+  messagingSenderId:"172042779786",
 
-  appId: "1:172042779786:web:ecdff9eaf4fee36eca8173",
+  appId:"1:172042779786:web:ecdff9eaf4fee36eca8173",
 
-  databaseURL: "https://grand-line-rpg-dcda9-default-rtdb.firebaseio.com"
+  databaseURL:"https://grand-line-rpg-dcda9-default-rtdb.firebaseio.com"
 
 };
 
-
-
-// ======================
-// INIT
-// ======================
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
 
-
-console.log("AUTH CHECK CARREGADO");
-
-
-
-const homeLink = document.getElementById("home-link");
+const homeLink =
+document.getElementById("home-link");
 
 
-
-// ======================
-// VERIFICAR LOGIN
-// ======================
-
-onAuthStateChanged(auth, (user)=>{
+async function verificarAuth(){
 
 
-  console.log("USUARIO ATUAL:", user);
+  await setPersistence(
+    auth,
+    browserLocalPersistence
+  );
 
 
-
-  if(!homeLink){
-
-    console.log("home-link não encontrado");
-
-    return;
-
-  }
+  onAuthStateChanged(auth,(user)=>{
 
 
-
-  if(user){
-
-
-    homeLink.innerHTML = "PERFIL";
-
-    homeLink.href = "perfil.html";
+    console.log("GUIA LOGIN:", user);
 
 
-  }else{
+    if(!homeLink) return;
 
 
-    homeLink.innerHTML = "INÍCIO";
+    if(user){
 
-    homeLink.href = "index.html";
+      homeLink.innerHTML = "PERFIL";
+      homeLink.href = "perfil.html";
+
+    }else{
+
+      homeLink.innerHTML = "INÍCIO";
+      homeLink.href = "index.html";
+
+    }
 
 
-  }
+  });
 
 
-});
+}
+
+
+verificarAuth();

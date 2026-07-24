9cd3e2ebeac9a0ee8bb3793ca7a7fbca4b7a4708
grandlinerpg <drivedesuna@gmail.com>
2026-07-24 14:25:51 -0300

    Update permissao.js

--- permissao.js
+++ permissao.js
@@ -1,11 +1,37 @@
 import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+import {
     getDatabase,
     ref,
     get
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 
-const db = getDatabase();
+const firebaseConfig = {
+
+apiKey:"AIzaSyC4kgy_L79WYFqr9XZhoDuZBfqG4AGTVUQ",
+
+authDomain:"grand-line-rpg-dcda9.firebaseapp.com",
+
+projectId:"grand-line-rpg-dcda9",
+
+storageBucket:"grand-line-rpg-dcda9.appspot.com",
+
+messagingSenderId:"172042779786",
+
+appId:"1:172042779786:web:ecdff9eaf4fee36eca8173",
+
+databaseURL:"https://grand-line-rpg-dcda9-default-rtdb.firebaseio.com"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
 
 
 window.verificarPermissao = async function(nivelNecessario){

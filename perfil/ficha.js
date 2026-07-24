063fc02995265cf1345d52dfe26892482780eb0f
grandlinerpg <drivedesuna@gmail.com>
2026-07-23 21:15:07 -0300

    Update ficha.js

--- perfil/ficha.js
+++ perfil/ficha.js
@@ -10,6 +10,53 @@ import {
 const app = getApp();
 const db = getDatabase(app);
 
+window.abrirEfeitoFicha = async function(id){
+
+
+    const snap =
+    await get(
+        ref(db,"efeitos/"+id)
+    );
+
+
+    if(!snap.exists()){
+
+        console.log("Efeito não encontrado");
+        return;
+
+    }
+
+
+    const efeito = snap.val();
+
+
+
+    document.getElementById("efeito-modal")
+    .style.display = "flex";
+
+
+    document.getElementById("efeito-nome")
+    .innerText =
+    efeito.nome || "-";
+
+
+    document.getElementById("efeito-emoji")
+    .innerText =
+    efeito.emoji || "✨";
+
+
+    document.getElementById("efeito-desc")
+    .innerText =
+    efeito.description || "";
+
+
+    document.getElementById("efeito-func")
+    .innerText =
+    efeito.funcionamento || "";
+
+
+};
+
 
 const fichaContainer = document.getElementById("ficha-container");
 let skillAtual = null;

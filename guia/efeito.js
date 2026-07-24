3dfae6ba22e2d71dc3948c497d50800e0274b4c1
grandlinerpg <drivedesuna@gmail.com>
2026-07-24 04:13:11 -0300

    Update efeito.js

--- guia/efeito.js
+++ guia/efeito.js
@@ -1,5 +1,5 @@
 import {
-    initializeApp
+    getApp
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
@@ -12,38 +12,9 @@ import {
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 
-const firebaseConfig = {
+const app = getApp();
 
-    apiKey:
-    "AIzaSyC4kgy_L79WYFqr9XZhoDuZBfqG4AGTVUQ",
-
-    authDomain:
-    "grand-line-rpg-dcda9.firebaseapp.com",
-
-    projectId:
-    "grand-line-rpg-dcda9",
-
-    storageBucket:
-    "grand-line-rpg-dcda9.appspot.com",
-
-    messagingSenderId:
-    "172042779786",
-
-    appId:
-    "1:172042779786:web:ecdff9eaf4fee36eca8173",
-
-    databaseURL:
-    "https://grand-line-rpg-dcda9-default-rtdb.firebaseio.com"
-
-};
-
-
-const app =
-initializeApp(firebaseConfig);
-
-
-const db =
-getDatabase(app);
+const db = getDatabase(app);
 
 
 const efeitoContainer =

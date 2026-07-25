61b2b3f5377d8aacb901abda0f8280ebf1ee4ab9
grandlinerpg <drivedesuna@gmail.com>
2026-07-25 09:01:07 -0300

    Create atividade.js

---
+++ guia/atividade.js
@@ -0,0 +1,302 @@
+import {
+    getApp
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+import {
+    getDatabase,
+    ref,
+    get
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+const app = getApp();
+
+const db = getDatabase(app);
+
+
+
+const atividadeContainer =
+document.getElementById("atividade-container");
+
+
+
+
+async function carregarAtividadeHTML(){
+
+
+    if(!atividadeContainer) return;
+
+
+    if(document.getElementById("atividade-modal")) return;
+
+
+
+    const caminhoAtividade =
+    window.location.pathname.includes("/guia/")
+    ? "atividade.html"
+    : "guia/atividade.html";
+
+
+
+    const res =
+    await fetch(caminhoAtividade);
+
+
+
+    const html =
+    await res.text();
+
+
+
+    atividadeContainer.insertAdjacentHTML(
+        "beforeend",
+        html
+    );
+
+
+
+    const close =
+    document.querySelector(".atividade-close");
+
+
+
+    close?.addEventListener("click",()=>{
+
+
+        document
+        .getElementById("atividade-modal")
+        .style.display="none";
+
+
+    });
+
+
+}
+
+
+
+
+
+window.abrirAtividade = async function(
+    atividadeId,
+    faccaoId
+){
+
+
+    await carregarAtividadeHTML();
+
+
+
+    const snap =
+    await get(
+        ref(
+            db,
+            `faccoes/${faccaoId}/atividades/${atividadeId}`
+        )
+    );
+
+
+
+    if(!snap.exists()){
+
+        console.log(
+            "Atividade não encontrada:",
+            atividadeId
+        );
+
+        return;
+
+    }
+
+
+
+    const atividade =
+    snap.val();
+
+
+
+    const modal =
+    document.getElementById("atividade-modal");
+
+
+
+    modal.style.display="flex";
+
+
+
+
+
+    document.querySelector(".atividade-title")
+    .innerText =
+    atividade.nome || "-";
+
+
+
+
+
+    document.querySelector(".atividade-desc")
+    .innerText =
+    atividade.description || "";
+
+
+
+
+
+    document.querySelector(".atividade-info")
+    .innerText =
+    atividade.funcionamento || "";
+
+
+
+
+
+    const extras =
+    document.querySelector(".atividade-extras");
+
+
+
+    extras.innerHTML = "";
+
+
+
+    Object.values(atividade.extras || {})
+    .forEach(extra=>{
+
+
+        extras.innerHTML += `
+
+            <div class="atividade-extra-box">
+
+                ${extra}
+
+            </div>
+
+        `;
+
+
+    });
+
+
+
+
+
+    const img =
+    document.querySelector(".atividade-img");
+
+
+
+    if(img){
+
+
+        if(atividade.img){
+
+
+            img.src =
+            `https://res.cloudinary.com/djh45admn/image/upload/v1784283109/${atividade.img}.jpg`;
+
+
+            img.style.display="block";
+
+
+        }else{
+
+
+            img.style.display="none";
+
+
+        }
+
+
+    }
+
+
+
+
+
+    // RECOMPENSA
+
+
+    const saldo =
+    document.querySelector(".recompensa-saldo");
+
+
+    const exp =
+    document.querySelector(".recompensa-exp");
+
+
+
+    if(saldo){
+
+        saldo.innerText =
+        atividade.recompensa?.saldo || 0;
+
+    }
+
+
+
+    if(exp){
+
+        exp.innerText =
+        atividade.recompensa?.exp || 0;
+
+    }
+
+
+
+
+
+    // EDITAR
+
+
+    const editBtn =
+    document.querySelector(".atividade-edit-btn");
+
+
+
+    if(editBtn){
+
+
+        const podeEditar =
+        await verificarPermissao(2);
+
+
+
+        if(podeEditar){
+
+
+            editBtn.style.display="block";
+
+
+
+            editBtn.onclick = ()=>{
+
+
+                window.atividadeAtual =
+                atividade;
+
+
+                abrirAtividadeEdit();
+
+
+            };
+
+
+        }else{
+
+
+            editBtn.style.display="none";
+
+            editBtn.onclick=null;
+
+
+        }
+
+
+    }
+
+
+
+};

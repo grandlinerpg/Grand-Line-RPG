d15f8fa1067cce00aecadb056c0bea3ae998136e
grandlinerpg <drivedesuna@gmail.com>
2026-09-09 10:41:36 -0300

    Fix formatting issues in index.js

--- bot/index.js
+++ bot/index.js
@@ -1,7 +1,7 @@
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const express = require('express');
 const axios = require('axios');
-const cron = require('node-cron');
+const cron = require('node-cron'); 
 
 const NUMERO_BOT = "5511918448331";
 const FIREBASE_URL = "https://grand-line-rpg-dcda9-default-rtdb.firebaseio.com";

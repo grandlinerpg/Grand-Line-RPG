4113cfd816d892041be296d6a95c49756c27343e
grandlinerpg <drivedesuna@gmail.com>
2026-09-10 11:01:29 -0300

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,5 @@
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
-const express = require('express');
+const express = require('express'); 
 const axios = require('axios');
 const cron = require('node-cron'); 
 
